2cbd301154b400ef37ae505da0008284ca00c37d
Berke Emrecan Arslan <beremaran@gmail.com>
2018-10-21 01:12:02 +0300

    create problem presentation classes

---
+++ Homework01/src/org/cbu/cse3111/mc/presentation/MCAction.java
@@ -0,0 +1,12 @@
+package org.cbu.cse3111.mc.presentation;
+
+import aima.core.agent.impl.DynamicAction;
+
+public class MCAction extends DynamicAction {
+
+	public MCAction(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Homework01/src/org/cbu/cse3111/mc/presentation/MCEnvironment.java
@@ -0,0 +1,5 @@
+package org.cbu.cse3111.mc.presentation;
+
+public class MCEnvironment {
+
+}
